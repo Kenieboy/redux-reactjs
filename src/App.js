1f3521b946f30c413d0a46595c1f5c84bb2320a9
Kenieboy <syntax0330@gmail.com>
2024-02-07 15:16:26 +0800

    This my first redux tutorial
    from freecodecamp yt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const disptach = useDispatch();
+
+  const decrement = () => {
+    disptach({ type: "DEC" });
+  };
+
+  const increment = () => {
+    disptach({ type: "INC" });
+  };
+
+  const addBy = () => {
+    disptach({ type: "ADD", payload: 10 });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+      <button onClick={addBy}>Add By 10</button>
     </div>
   );
 }
